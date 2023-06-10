davidsaOpenu/xv6-ns

cgroup memory controller - memory.min
Add support for memory.min
When declaring memory.min updates a counter of saved pages that can only be assigned to this group.
If there are not enough free pages to save the statement you will fail.
If you try to allocate memory, such as creating a process, but there are no unsaved pages [the number of free pages equals the number of saved pages] the request will be rejected.
When allocating memory to a group for which pages have been saved, the number of saved pages will decrease.

Change-Id: I13b1b5c417c97a89067fb647608569694f5269e2

proc.h
@@ -101,6 +101,11 @@ void proc_unlock();
  */
 struct cgroup *proc_get_cgroup(void);
 
+/**
+ * Update number of memory pages to protect for cgroup after dealloc memory .
+ */
+void update_protect_mam(struct cgroup* cgroup, int oldsz, int newsz);
+
 // Process memory is laid out contiguously, low addresses first:
 //   text
 //   original data and bss

davidsaOpenu/xv6

proc.h
@@ -103,6 +103,11 @@ void proc_unlock();
  */
 struct cgroup *proc_get_cgroup(void);
 
+/**
+ * Update number of memory pages to protect for cgroup after dealloc memory .
+ */
+void update_protect_mem(struct cgroup* cgroup, int oldsz, int newsz);
+
 // Process memory is laid out contiguously, low addresses first:
 //   text
 //   original data and bss