03efb69313c0e1e8fd361338571695e242d1980c
Dolev Z <dolev461@gmail.com>
2023-06-13 11:35:47 +0300

    Bashate
    Fixed run-ci.sh bashate comments.
    Mostly indentations and long lines fixes.
    
    Change-Id: I50dad91e4159b2d8784ddd8f053d31eee91e696d

--- run-ci.sh
+++ run-ci.sh
@@ -8,18 +8,19 @@ set -uo # nounset,pipefail
 #########################################################################
 # clang-format
 # clang-format -style=google -dump-config > .clang-format
-changed_files=$(find . -regex ".*\.[c|h]$" -exec clang-format -i {} \; -exec git diff --name-only {} \;)
+changed_files=$(find . -regex ".*\.[c|h]$" -exec clang-format -i {} \; \
+    -exec git diff --name-only {} \;)
 
 if [ -n "$changed_files" ]; then
-  echo "Files were changed by clang-format:"
-  echo "$changed_files"
-  git diff --color=always &> required_changes.txt
-  cat required_changes.txt
-  exit 1
+    echo "Files were changed by clang-format:"
+    echo "$changed_files"
+    git diff --color=always &> required_changes.txt
+    cat required_changes.txt
+    exit 1
 fi
 
 #########################################################################
-# inatll and run cpp style checker
+# install and run cpp style checker
 python3 -m venv /tmp/myenv
 . /tmp/myenv/bin/activate
 pip install cpplint
@@ -31,8 +32,8 @@ find . -regex ".*\.[c|h]$" | xargs cpplint --filter=$FILTERS
 
 # TODO: remove when run-ci.sh passes all tests
 if [ $? -ne 0 ]; then
-  echo "Cpplint comments are not completed."
-  exit 1
+    echo "Cpplint comments are not completed."
+    exit 1
 fi
 
 # TODO: remove when run-ci.sh passes all tests
@@ -40,17 +41,24 @@ if false; then
 ########################################################################
 # install and run bashate
 pip install bashate
-find . -name "*.sh"  -exec bashate {} \;
+find . -iname "*.sh" -exec bashate {} \; > /tmp/bashate-out
+cat /tmp/bashate-out
+
+grep "warning(s) found" /tmp/bashate-out 1>/dev/null
+if [ $? -eq 0 ]; then
+    echo "Bashate comments are not completed."
+    exit 1
+fi
 
 ########################################################################
 # run static analyzer
 ERROR_CODE=20
 cppcheck --error-exitcode=${ERROR_CODE} \
         --enable=portability,information,performance,warning \
-	--inconclusive --xml --xml-version=2 . 2> cppcheck.xml
+    --inconclusive --xml --xml-version=2 . 2> cppcheck.xml
 if [ $? -eq  $ERROR_CODE ]; then
-  cppcheck-htmlreport --file=cppcheck.xml --report-dir=cppcheck-report \
-	  --source-dir=. --title="Cppcheck Report"
+    cppcheck-htmlreport --file=cppcheck.xml --report-dir=cppcheck-report \
+        --source-dir=. --title="Cppcheck Report"
 fi
 
 fi #if false
@@ -69,8 +77,8 @@ LOG_FILE="mylog.txt"
 #  Last verification
 lines=$(tail -5 $LOG_FILE | grep  "ALL TESTS PASSED" | wc -l)
 if [ $lines -ne 1 ]; then
-   echo "ALL TESTS PASSED string was not found"
-   exit 1
+    echo "ALL TESTS PASSED string was not found"
+    exit 1
 fi
 
 echo "SUCCESS"
