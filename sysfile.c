davidsaOpenu/xv6-ns

Added exclusive file open - Refactor test_mem_stat in cgroupstests.c
Trello issue: https://trello.com/c/0WZJm08d/122-refactor-testmemstat-in-cgroupstestsc

Change-Id: I291a51c56f9dbe10e63e86325fbce206e4ac6b04

sysfile.c
@@ -274,7 +274,12 @@ bad:
 }
 
 static struct inode*
-createmount(char *path, short type, short major, short minor, struct mount **mnt)
+createmount(char *path,
+            short type,
+            short major,
+            short minor,
+            struct mount **mnt,
+            int omode)
 {
   uint off;
   struct inode *ip, *dp;
@@ -286,6 +291,9 @@ createmount(char *path, short type, short major, short minor, struct mount **mnt
 
   if((ip = dirlookup(dp, name, &off)) != 0){
     iunlockput(dp);
+    // The path has already been created
+    if (omode & O_EXCL)
+      return (void *)EEXIST;
     ilock(ip);
     if(type == T_FILE && ip->type == T_FILE)
       return ip;
@@ -323,7 +331,7 @@ static struct inode*
 create(char *path, short type, short major, short minor)
 {
   struct mount *mnt;
-  struct inode *res = createmount(path, type, major, minor, &mnt);
+  struct inode *res = createmount(path, type, major, minor, &mnt, O_CREATE);
   if (res != 0) {
     mntput(mnt);
   }
@@ -352,10 +360,14 @@ sys_open(void)
   }
 
   if(omode & O_CREATE){
-    ip = createmount(path, T_FILE, 0, 0, &mnt);
+    ip = createmount(path, T_FILE, 0, 0, &mnt, omode);
+
     if(ip == 0){
       end_op();
       return -1;
+    } else if((int)ip == EEXIST){
+      end_op();
+      return EEXIST;
     }
   } else {
     if((ip = nameimount(path, &mnt)) == 0){

davidsaOpenu/xv6

sysfile.c
@@ -234,7 +234,7 @@ bad:
 }
 
 static struct vfs_inode *createmount(char *path, short type, short major,
-                                     short minor, struct mount **mnt) {
+                                     short minor, struct mount **mnt, int omode) {
   uint off;
   struct vfs_inode *ip, *dp;
   char name[DIRSIZ];
@@ -244,6 +244,8 @@ static struct vfs_inode *createmount(char *path, short type, short major,
 
   if ((ip = dp->i_op.dirlookup(dp, name, &off)) != 0) {
     dp->i_op.iunlockput(dp);
+    // The path has already been created
+    if (omode & O_EXCL) return (void *)EEXIST;
     ip->i_op.ilock(ip);
     if (type == T_FILE && ip->type == T_FILE) return ip;
     ip->i_op.iunlockput(ip);
@@ -283,7 +285,7 @@ static struct vfs_inode *createmount(char *path, short type, short major,
 static struct vfs_inode *create(char *path, short type, short major,
                                 short minor) {
   struct mount *mnt;
-  struct vfs_inode *res = createmount(path, type, major, minor, &mnt);
+  struct vfs_inode *res = createmount(path, type, major, minor, &mnt, O_CREATE);
   if (res != 0) {
     mntput(mnt);
   }
@@ -309,10 +311,13 @@ int sys_open(void) {
   }
 
   if (omode & O_CREATE) {
-    ip = createmount(path, T_FILE, 0, 0, &mnt);
+    ip = createmount(path, T_FILE, 0, 0, &mnt, omode);
     if (ip == 0) {
       end_op();
       return -1;
+    } else if((int)ip == EEXIST){
+      end_op();
+      return EEXIST;
     }
   } else {
     if ((ip = vfs_nameimount(path, &mnt)) == 0) {