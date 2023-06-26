0100ea7b822a1fdcda709d5089696abe745afef2
David Sariel <davidsa@openu.ac.il>
2023-06-26 08:32:52 +0300

    Re-enable linters and code analyzers
    Everything in runtests.exp was enabled and passing.
    Time to reenable linters et al.
    
    Note: reverting changes acceidentay slipped into mkfs.c.
    
    Change-Id: I90dee69ee319a29166ef418d2a69add726253aab

--- mkfs.c
+++ mkfs.c
@@ -1,4 +1,3 @@
-#include <arpa/inet.h>
 #include <assert.h>
 #include <fcntl.h>
 #include <stdio.h>
@@ -46,6 +45,24 @@ uint ialloc(ushort type);
 void iappend(uint inum, void *p, int n);
 
 // convert to intel byte order
+ushort xshort(ushort x) {
+  ushort y;
+  uchar *a = (uchar *)&y;
+  a[0] = x;
+  a[1] = x >> 8;
+  return y;
+}
+
+uint xint(uint x) {
+  uint y;
+  uchar *a = (uchar *)&y;
+  a[0] = x;
+  a[1] = x >> 8;
+  a[2] = x >> 16;
+  a[3] = x >> 24;
+  return y;
+}
+
 void printusageexit(void) {
   fprintf(stderr, "Usage: mkfs fs.img <is_internal (0/1)> files...\n");
   exit(1);
@@ -86,16 +103,16 @@ int main(int argc, char *argv[]) {
   nmeta = 2 + nlog + ninodeblocks + nbitmap;
   nblocks = fssize - nmeta;
 
-  sb.size = (uint)fssize;
-  sb.nblocks = (uint)nblocks;
-  sb.vfs_sb.ninodes = NINODES;
-  sb.nlog = (uint)nlog;
-  sb.logstart = 2;
-  sb.inodestart = (uint)(2 + nlog);
-  sb.bmapstart = (uint)(2 + nlog + ninodeblocks);
+  sb.size = xint(fssize);
+  sb.nblocks = xint(nblocks);
+  sb.ninodes = xint(NINODES);
+  sb.nlog = xint(nlog);
+  sb.logstart = xint(2);
+  sb.inodestart = xint(2 + nlog);
+  sb.bmapstart = xint(2 + nlog + ninodeblocks);
 
   printf(
-      "nmeta %d (boot, super, log blocks %d inode blocks %d, bitmap blocks %d) "
+      "nmeta %d (boot, super, log blocks %u inode blocks %u, bitmap blocks %u) "
       "blocks %d total %d\n",
       nmeta, nlog, ninodeblocks, nbitmap, nblocks, fssize);
 
@@ -111,12 +128,12 @@ int main(int argc, char *argv[]) {
   assert(rootino == ROOTINO);
 
   bzero(&de, sizeof(de));
-  de.inum = (ushort)rootino;
+  de.inum = xshort(rootino);
   strcpy(de.name, ".");
   iappend(rootino, &de, sizeof(de));
 
   bzero(&de, sizeof(de));
-  de.inum = (ushort)rootino;
+  de.inum = xshort(rootino);
   strcpy(de.name, "..");
   iappend(rootino, &de, sizeof(de));
 
@@ -137,7 +154,7 @@ int main(int argc, char *argv[]) {
     inum = ialloc(T_FILE);
 
     bzero(&de, sizeof(de));
-    de.inum = (ushort)inum;
+    de.inum = xshort(inum);
     strncpy(de.name, argv[i], DIRSIZ);
     iappend(rootino, &de, sizeof(de));
 
@@ -148,9 +165,9 @@ int main(int argc, char *argv[]) {
 
   // fix size of root inode dir
   rinode(rootino, &din);
-  off = din.size;
+  off = xint(din.size);
   off = ((off / BSIZE) + 1) * BSIZE;
-  din.size = off;
+  din.size = xint(off);
   winode(rootino, &din);
 
   balloc(freeblock);
@@ -208,9 +225,9 @@ uint ialloc(ushort type) {
   struct dinode din;
 
   bzero(&din, sizeof(din));
-  din.vfs_dinode.type = (short)type;
-  din.vfs_dinode.nlink = 1;
-  din.size = 0;
+  din.type = xshort(type);
+  din.nlink = xshort(1);
+  din.size = xint(0);
   winode(inum, &din);
   return inum;
 }
@@ -225,7 +242,7 @@ void balloc(int used) {
   for (i = 0; i < used; i++) {
     buf[i / 8] = buf[i / 8] | (0x1 << (i % 8));
   }
-  printf("balloc: write bitmap block at sector 0x%x\n", sb.bmapstart);
+  printf("balloc: write bitmap block at sector %d\n", sb.bmapstart);
   wsect(sb.bmapstart, buf);
 }
 
@@ -240,26 +257,26 @@ void iappend(uint inum, void *xp, int n) {
   uint x;
 
   rinode(inum, &din);
-  off = din.size;
+  off = xint(din.size);
   // printf("append inum %d at off %d sz %d\n", inum, off, n);
   while (n > 0) {
     fbn = off / BSIZE;
     assert(fbn < MAXFILE);
     if (fbn < NDIRECT) {
-      if (din.addrs[fbn] == 0) {
-        din.addrs[fbn] = freeblock++;
+      if (xint(din.addrs[fbn]) == 0) {
+        din.addrs[fbn] = xint(freeblock++);
       }
-      x = din.addrs[fbn];
+      x = xint(din.addrs[fbn]);
     } else {
-      if (din.addrs[NDIRECT] == 0) {
-        din.addrs[NDIRECT] = freeblock++;
+      if (xint(din.addrs[NDIRECT]) == 0) {
+        din.addrs[NDIRECT] = xint(freeblock++);
       }
-      rsect(din.addrs[NDIRECT], (char *)indirect);
+      rsect(xint(din.addrs[NDIRECT]), (char *)indirect);
       if (indirect[fbn - NDIRECT] == 0) {
-        indirect[fbn - NDIRECT] = freeblock++;
-        wsect(din.addrs[NDIRECT], (char *)indirect);
+        indirect[fbn - NDIRECT] = xint(freeblock++);
+        wsect(xint(din.addrs[NDIRECT]), (char *)indirect);
       }
-      x = indirect[fbn - NDIRECT];
+      x = xint(indirect[fbn - NDIRECT]);
     }
     n1 = min(n, (fbn + 1) * BSIZE - off);
     rsect(x, buf);
@@ -269,6 +286,6 @@ void iappend(uint inum, void *xp, int n) {
     off += n1;
     p += n1;
   }
-  din.size = off;
+  din.size = xint(off);
   winode(inum, &din);
 }
