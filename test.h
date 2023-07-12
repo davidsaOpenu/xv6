4498ddce150856d0c5b0a45f7fa3139a50adcf98
alondoitsh <alondoitsh@gmail.com>
2023-07-12 22:46:18 +0300

    resolve duplications in test.h and obj_fs_tests.h
    trello issue: https://trello.com/c/EzvEuj9A/115-maman-11-resolve-duplication-bw-xv6-objfs-testh-and-xv6-objfs-objfstestsh
    
    Change-Id: I2bb630289e1eb6185716aac572a6f35792ae82e7

--- test.h
+++ test.h
@@ -1,6 +1,5 @@
 #ifndef XV6_TEST_H
 #define XV6_TEST_H
-
 /**
  Defines a simple test library.
  To define a new test use `TEST`. For example:
@@ -20,110 +19,7 @@
  */
 
 #include "user.h"
-
-#define REMOVE_2_ADDITIONAL_CHARS 2
-
-void print_error(const char* name, unsigned long int x, unsigned long int y,
-                 const char* file, int line) {
-  for (int i = 0;
-       i < strlen(name) + strlen("[RUNNING] ") + REMOVE_2_ADDITIONAL_CHARS; i++)
-    printf(1, "\b");
-  printf(1, "[FAILED] %s - expected %d but got %d (%s:%d)\n", name, x, y, file,
-         line);
-}
-
-#define TEST(test_name) void test_name(const char* name)
-
-#define run_test(test_name)                                         \
-  if (failed == 0) {                                                \
-    printf(1, "[RUNNING] %s", #test_name);                          \
-    test_name(#test_name);                                          \
-  }                                                                 \
-  if (failed == 0) {                                                \
-    for (int i = 0; i < strlen(#test_name) + strlen("[RUNNING] ") + \
-                            REMOVE_2_ADDITIONAL_CHARS;              \
-         i++)                                                       \
-      printf(1, "\b");                                              \
-    printf(1, "[DONE] %s   \n", #test_name);                        \
-  }
-
-// Prints the exectuion message differently.
-#define run_test_break_msg(test_name)        \
-  if (failed == 0) {                         \
-    printf(1, "[RUNNING] %s\n", #test_name); \
-    test_name(#test_name);                   \
-  }                                          \
-  if (failed == 0) printf(1, "[DONE] %s\n", #test_name);
-
-/**
- * NUMERIC VALIDATIONS
- */
-
-#define ASSERT_UINT_EQ(x, y)                     \
-  if ((x) != (y)) {                              \
-    print_error(name, x, y, __FILE__, __LINE__); \
-    failed = 1;                                  \
-    return;                                      \
-  }
-
-#define EXPECT_UINT_EQ(x, y)                     \
-  if ((x) != (y)) {                              \
-    print_error(name, x, y, __FILE__, __LINE__); \
-    failed = 1;                                  \
-  }
-
-/**
- * BOOLEAN VALIDATIONS
- */
-
-#define ASSERT_TRUE(x)                                                      \
-  if (!(x)) {                                                               \
-    for (int i = 0;                                                         \
-         i < strlen(#x) + strlen("[RUNNING] ") + REMOVE_2_ADDITIONAL_CHARS; \
-         i++)                                                               \
-      printf(1, "\b");                                                      \
-    printf(1, "[FAILED] %s - expected true for " #x " (%s:%d)\n", name,     \
-           __FILE__, __LINE__);                                             \
-    failed = 1;                                                             \
-    return;                                                                 \
-  }
-
-#define EXPECT_TRUE(x)                                                      \
-  if (!(x)) {                                                               \
-    for (int i = 0;                                                         \
-         i < strlen(#x) + strlen("[RUNNING] ") + REMOVE_2_ADDITIONAL_CHARS; \
-         i++)                                                               \
-      printf(1, "\b");                                                      \
-    printf(1, "[FAILED] %s - expected true for " #x " (%s:%d)\n", name,     \
-           __FILE__, __LINE__);                                             \
-    failed = 1;                                                             \
-  }
-
-#define ASSERT_FALSE(x)                                                     \
-  if (x) {                                                                  \
-    for (int i = 0;                                                         \
-         i < strlen(#x) + strlen("[RUNNING] ") + REMOVE_2_ADDITIONAL_CHARS; \
-         i++)                                                               \
-      printf(1, "\b");                                                      \
-    printf(1, "[FAILED] %s - expected false for " #x "(%s:%d)\n", name,     \
-           __FILE__, __LINE__);                                             \
-    failed = 1;                                                             \
-    return;                                                                 \
-  }
-
-#define ASSERT_EQ(x, y) ASSERT_TRUE(x == y)
-#define ASSERT_NE(x, y) ASSERT_FALSE(x == y)
-#define ASSERT_GE(x, y) ASSERT_TRUE(x >= y)
-
-#define EXPECT_FALSE(x)                                                     \
-  if (x) {                                                                  \
-    for (int i = 0;                                                         \
-         i < strlen(#x) + strlen("[RUNNING] ") + REMOVE_2_ADDITIONAL_CHARS; \
-         i++)                                                               \
-      printf(1, "\b");                                                      \
-    printf(1, "[FAILED] %s - expected false for " #x "(%s:%d)\n", name,     \
-           __FILE__, __LINE__);                                             \
-    failed = 1;                                                             \
-  }
+#define PRINT(...) printf(1, __VA_ARGS__)
+#include "test_commons.h"
 
 #endif /* XV6_TEST_H */
