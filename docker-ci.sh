068aa43608b74c967ce9c1b4032253b8a2d248b0
Dor-sketch <dor.pascal@mail.huji.ac.il>
2023-08-19 03:33:53 +0300

    Fix init bug and add Dockerfile for Ubuntu 16.04
    This commit fixes a bug in the init process that was causing issues with system startup (for details see: https://trello.com/c/Xf8bEZLJ).
    
    Additionally, a Dockerfile and script have been added to support building and testing the xv6 image in a Docker container.
    
    The Dockerfile includes all necessary dependencies and packages for building and running the xv6 image on Ubuntu 16.04.
    
    Note: linting tools build flag was added to skip 16.04 and save time (see docker-ci.sh for details).
    
    The script can be used to build and test the image in a Docker container, and includes options for specifying the version of Ubuntu to use.
    
    These changes should make it easier to build and test the xv6 image in a consistent and reproducible environment.
    
    Change-Id: I6008b68cbe75b4166348d44324d65810d9264ffb

---
+++ docker-ci.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# use this script to build and test the xv6 image in a docker container.
+# if <ubuntu-version> is not specified, it defaults to 22.04.
+# Run this script with: ./docker-ci.sh ...
+#                                      build       <ubuntu-version>
+#                                      test        <ubuntu-version>
+#                                      interactive <ubuntu-version> <command>
+
+# Set the default version of Ubuntu to use
+if [ -z "$2" ]; then
+    UBUNTU_VERSION="22.04"
+elif [ "$2" == "16.04" ]; then
+    UBUNTU_VERSION="16.04"
+elif [ "$2" == "22.04" ]; then
+    UBUNTU_VERSION="22.04"
+else
+    echo "Invalid Ubuntu version: $2"
+    exit 1
+fi
+
+# Set the Dockerfile based on the Ubuntu version
+if [ "$UBUNTU_VERSION" == "16.04" ]; then
+    DOCKERFILE="Dockerfile_16.04.dockerfile"
+    IMAGE_NAME="xv6-test-image-16-04"
+else
+    DOCKERFILE="Dockerfile"
+    IMAGE_NAME="xv6-test-image"
+fi
+
+#########################################################################
+# build the docker image if it does not exist
+
+# Check if the Docker image exists
+if ! docker image inspect $IMAGE_NAME &> /dev/null; then
+    echo "Docker image $IMAGE_NAME not found. Building..."
+    docker build --build-arg USERNAME=$(whoami) \
+                --build-arg GRPNAME=$(id -gn) \
+                --build-arg UID=$(id -u) \
+                --build-arg GID=$(id -g) -t $IMAGE_NAME -f $DOCKERFILE .
+    exit 0
+elif [ "$1" == "build" ]; then
+    echo "Docker image $IMAGE_NAME already exists. \
+    Use 'docker rmi $IMAGE_NAME' to remove it."
+    exit 0
+fi
+
+#########################################################################
+# test the docker image using the run-ci.sh script or intaractive mode
+
+# Check the first argument to determine what to do
+if [ "$1" == "test" ]; then
+    # Run tests
+    docker run --mount type=bind,source="$(pwd)",target=/home/$(whoami)/xv6 \
+                --rm $IMAGE_NAME \
+                /home/$(whoami)/xv6/run-ci.sh
+elif [ "$1" == "interactive" ]; then
+    # Run interactive command
+    if [ -z "$3" ]; then
+        echo "Usage: $0 interactive <ubuntu-version> <command>"
+        exit 1
+    fi
+    docker run -it \
+        --mount type=bind,source="$(pwd)",target=/home/$(whoami)/xv6 \
+        --rm $IMAGE_NAME $3
+else
+    echo "Invalid command: $1"
+    exit 1
+fi
