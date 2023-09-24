davidsaOpenu/xv6

commit 01e2b0241902509cd85068ffe4761fb9e1227dfe

Fixed unstable pouch_start_test test issue
https://trello.com/c/7H8OZP9o/113-unstable-pouch-tests

Change-Id: I475caf21da73f2ffad3dd3e33272c8c3d4aa610c

kmount.c
@@ -261,13 +261,13 @@ static struct mount_list *shallowcopyactivemounts(struct mount **newcwdmount) {
   struct mount_list *head = 0;
   struct mount_list *entry = myproc()->nsproxy->mount_ns->active_mounts;
   struct mount_list *prev = 0;
-  while (entry != 0) {
+  while (entry != 0 && entry->mnt.mountpoint != 0) {
     struct mount_list *newentry = allocmntlist();
     if (head == 0) {
       head = newentry;
     }
     newentry->mnt.ref = 1;
-    newentry->mnt.mountpoint =
+   	newentry->mnt.mountpoint =
         entry->mnt.mountpoint->i_op.idup(entry->mnt.mountpoint);
     newentry->mnt.parent = 0;
     newentry->mnt.dev = entry->mnt.dev;

console.c
@@ -32,6 +32,7 @@ typedef struct device_lock {
 
 typedef struct tty {
   int flags;
+  struct spinlock lock;
 } tty;
 tty tty_table[MAX_TTY];
 
@@ -277,6 +278,17 @@ int ttyread(struct vfs_inode *ip, int n, vector *dstvector) {
   if (tty_table[ip->minor].flags & DEV_CONNECT) {
     return consoleread(ip, n, dstvector);
   }
+
+  if(tty_table[ip->minor].flags & DEV_ATTACH)
+  {
+    ip->i_op.iunlock(ip);
+    acquire(&tty_table[ip->minor].lock);
+    sleep(&tty_table[ip->minor], &tty_table[ip->minor].lock);
+    //after wakeup has been called
+    release(&tty_table[ip->minor].lock);
+    ip->i_op.ilock(ip);
+  }
+  
   return -1;
 }
 
@@ -307,6 +319,12 @@ void consoleinit(void) {
   devsw[CONSOLE_MAJOR].read = ttyread;
   tty_table[CONSOLE_MINOR].flags = DEV_CONNECT;
 
+  //To state that the console tty is also attached
+  //this will make the console sleep whilre we are connected to another tty.
+  tty_table[CONSOLE_MINOR].flags |= DEV_ATTACH;
+  initlock(&tty_table[CONSOLE_MINOR].lock, "ttyconsole");
+
+
   cons.locking = 1;
 
   ioapicenable(IRQ_KBD, 0);
@@ -323,6 +341,10 @@ void ttyinit(void) {
 void tty_disconnect(struct vfs_inode *ip) {
   tty_table[ip->minor].flags &= ~(DEV_CONNECT);
   tty_table[CONSOLE_MINOR].flags |= DEV_CONNECT;
+
+  //wakeup the console (it is sleeping now while being attached)
+  wakeup(&tty_table[CONSOLE_MINOR]);
+
   consoleclear();
   cprintf("Console connected\n");
 }
@@ -332,14 +354,18 @@ void tty_connect(struct vfs_inode *ip) {
   for (int i = CONSOLE_MINOR; i < MAX_TTY; i++) {
     if (ip->minor != i) {
       tty_table[i].flags &= ~(DEV_CONNECT);
-    }
-  }
-  consoleclear();
-  cprintf("\ntty%d connected\n", ip->minor - (CONSOLE_MINOR + 1));
+      }
+   }
+   consoleclear();
+   cprintf("\ntty%d connected\n", ip->minor - (CONSOLE_MINOR + 1));
+  
+  //Wakeup the processes that slept on ttyread()
+  wakeup(&tty_table[ip->minor]);
 }
 
 void tty_attach(struct vfs_inode *ip) {
   tty_table[ip->minor].flags |= DEV_ATTACH;
+  initlock(&tty_table[ip->minor].lock, "tty");
 }
 
 void tty_detach(struct vfs_inode *ip) {

runtests.exp
@@ -11,7 +11,7 @@ proc pid_check { argv expect_str} {
 
     send "pid\n"
     log_file -noappend [lindex $argv 0]
-    set timeout 3
+    set timeout 7
     expect {
        expect_str
     }
@@ -458,7 +458,7 @@ set running [ioctl_syscall_test $argv]
 set running [cgroupstests $argv]
 
 #basic tests
-#set running [pouch_start_test $argv]
+set running [pouch_start_test $argv]
 #set running [pouch_connect_test $argv]
 #set running [pouch_disconnect_test $argv]
 #set running [pouch_destroy_test $argv]

commit e0905912dfa126860e1f2f85b057d54d142f0bbc

Fixed the bug that caused binaries inside containers to crash at address 0x8
The bug introduced in commit 01e2b0241902509cd85068ffe4761fb9e1227dfe which missed the case in which newcwdmount is not overriden and stays zero which makes myproc()->cwdmount to equal 0. later on, when calling open() inside the forked process in the container it called vfs_nameimount which called vfs_namex, which called mntdup(myproc()->cwdmount) and thus incremented 0x8 by 1. This altered the memory resides there, which in the case of the executed binaries was one the instructions in the executable area, thus resulting in an unexpected behaviour.

defs.h
@@ -92,6 +92,7 @@ struct vfs_inode* vfs_nameimount(char*, struct mount**);
 struct vfs_inode* vfs_nameiparent(char*, char*);
 struct vfs_inode* vfs_nameiparentmount(char*, char*, struct mount**);
 int vfs_namecmp(const char*, const char*);
+int vfs_namencmp(const char *s, const char *t, int length);
 
 // mount.c
 void mntinit(void);

kmount.c
@@ -294,9 +294,10 @@ struct mount_list *copyactivemounts(void) {
   struct mount_list *newentry = shallowcopyactivemounts(&newcwdmount);
   fixparents(newentry);
   release(&myproc()->nsproxy->mount_ns->lock);
-
-  myproc()->cwdmount = mntdup(newcwdmount);
-  mntput(oldcwdmount);
+  if (newcwdmount != 0){
+    myproc()->cwdmount = mntdup(newcwdmount);
+    mntput(oldcwdmount);
+  }
   return newentry;
 }
 

vfs_file.c
@@ -70,7 +70,7 @@ void vfs_fileclose(struct vfs_file *f) {
 
 // Get metadata about file f.
 int vfs_filestat(struct vfs_file *f, struct stat *st) {
-  if (f->type == FD_INODE) {
+  if (f->type == FD_INODE && f->ip != 0) {
     f->ip->i_op.ilock(f->ip);
     f->ip->i_op.stati(f->ip, st);
     f->ip->i_op.iunlock(f->ip);

vfs_fs.c
@@ -78,9 +78,9 @@ static struct vfs_inode *vfs_namex(char *path, int nameiparent, char *name,
 
     mntinum = ip->inum;
     ip->i_op.iunlockput(ip);
-
-    if ((!vfs_namecmp(name, "..")) && (curmount->dev != ROOTDEV) &&
-        ((mntinum == ROOTINO) || (mntinum == OBJ_ROOTINO))) {
+    if ((!vfs_namencmp(name, "..", 3)) && curmount != 0 && (curmount->dev != ROOTDEV) &&
+        ((mntinum == ROOTINO) || (mntinum == OBJ_ROOTINO)) && curmount->mountpoint != 0 &&
+        curmount->mountpoint->i_op.dirlookup != 0) {
       nextmount = mntdup(curmount->parent);
       mntinum =
           curmount->mountpoint->i_op.dirlookup(curmount->mountpoint, "..", 0)
@@ -153,3 +153,5 @@ struct vfs_inode *vfs_nameimount(char *path, struct mount **mnt) {
 }
 
 int vfs_namecmp(const char *s, const char *t) { return strncmp(s, t, DIRSIZ); }
+
+int vfs_namencmp(const char *s, const char *t, int length) { return strncmp(s, t, length); }