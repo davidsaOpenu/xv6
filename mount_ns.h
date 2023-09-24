ff46bdadf72724b8470b014bae4f67647c66d0da
Nahman Khayet <nkhayet@gmail.com>
2023-09-25 00:24:06 +0300

    Added basic implementation for bind mount - including apply_binds (func that replaces the actual path) and its usage in sys_open
    https://trello.com/c/KFLCTNBC/151-support-bind-mount-functionality
    
    Change-Id: Ic9734ca2b4ee56fd7943ce79cd06b4dc715c009f

--- mount_ns.h
+++ mount_ns.h
@@ -2,11 +2,24 @@
 #define XV6_MOUNT_NS_H
 
 #include "mount.h"
+#include "param.h"
 #include "spinlock.h"
 
 struct mount_list {
   struct mount mnt;
   struct mount_list* next;
+};
+
+/* A bind table, acts as a linked list where each node is a pair
+ * of a source path and a target path,
+ * refrenced when resolving paths to redirect access from source to target
+ * A bind table belongs to a mount_ns, so that the entire mount namespace
+ * would share the same view of the file system
+ */
+struct bind_mount_list {
+  char source[MAX_PATH_LENGTH];
+  char target[MAX_PATH_LENGTH];
+  struct bind_mount_list* next;
 };
 
 /* Note the structure of active_mounts. New mounts are always added to the
@@ -19,6 +32,8 @@ struct mount_ns {
   struct spinlock lock;  // protects active_mounts
   struct mount* root;
   struct mount_list* active_mounts;
+  struct bind_mount_list* bind_table;  // Table of binds shared across all
+                                       // processes inside the mount namespace
 };
 
 #endif /* XV6_MOUNT_NS_H */
