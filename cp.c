4e56934951d655b7102c92efd67122687f2b9a1f
Guy David <guyda96@gmail.com>
2024-01-16 23:25:39 +0200

    cp: Refactor and fix edge cases
    - Fixed leak of file descriptors
    - Refactored for readability
    - Fixed an edge case when copying a path with an extra slash
    
    https://trello.com/c/PgC4wkAt/156-creating-a-cp-executable
    
    Change-Id: I695933ef0c5b69bc2960466020a31adafc205323
    Signed-off-by: Guy David <guy.david@nextsilicon.com>

--- cp.c
+++ cp.c
@@ -5,45 +5,101 @@
 #include "types.h"
 #include "user.h"
 
-char* fmtname(char* path) {
-  char* p;
+static int cp(const char* src, const char* target);
 
-  // Find first character after last slash.
-  for (p = path + strlen(path); p >= path && *p != '/'; p--) {
-  }
-  p++;
+static int join_paths(char* dest, int dest_size, const char* path1,
+                      const char* path2) {
+  int path1_len = strlen(path1);
+  if (path1_len >= dest_size) return -1;
+  memmove(dest, path1, path1_len);
+  dest += path1_len;
+  dest_size -= path1_len;
 
-  return p;
-}
+  if (dest[0] != '/') {
+    if (dest_size <= 1) return -1;
+    dest[0] = '/';
+    dest++;
+    dest_size--;
+  }
 
-int copy_file(const char* src, const char* target) {
-  int fds, fdt, n;
-  char buf[512];
-  if ((fds = open(src, O_RDONLY)) < 0) {
-    printf(2, "cant locate %s\n", src);
-    return -1;
-  }
-  if ((fdt = open(target, O_CREATE | O_WRONLY)) < 0) {
-    printf(2, "cant create %s, make sure the entire path exists\n", target);
-    return -1;
-  }
-  while ((n = read(fds, buf, sizeof(buf))) > 0) {
-    if (write(fdt, buf, n) != n) {
-      printf(1, "cp: write error\n");
-      exit(1);
-    }
-  }
+  int path2_len = strlen(path2);
+  if (path2_len >= dest_size) return -1;
+
+  memmove(dest, path2, path2_len);
+  dest += path2_len;
+
+  *dest = '\0';
   return 0;
 }
 
-int cp(const char* src, const char* target) {
-  int fd;
-  struct stat st;
+static int copy_file(int fd, const char* target) {
+  int fdt, n;
+  char buf[512];
+
+  if ((fdt = open(target, O_CREATE | O_WRONLY)) < 0) {
+    printf(2, "can't create %s, make sure the entire path exists\n", target);
+    return -1;
+  }
+
+  while ((n = read(fd, buf, sizeof(buf))) > 0) {
+    if (write(fdt, buf, n) != n) {
+      printf(2, "cp: write error\n");
+      return -1;
+    }
+  }
+
+  return 0;
+}
+
+static int copy_dir(int fd, const char* src, const char* target) {
+  /* Verify target is a directory or create it. */
+  int target_fd = open(target, O_RDONLY);
+  if (target_fd >= 0) {
+    struct stat st;
+    if (fstat(target_fd, &st) < 0) {
+      printf(2, "cannot stat %s\n", target);
+      close(target_fd);
+      return -1;
+    }
+
+    if (st.type != T_DIR) {
+      printf(2, "target %s is not a directory\n", target);
+      close(target_fd);
+      return -1;
+    }
+  } else if (mkdir(target) < 0) {
+    return -1;
+  }
+
   struct dirent de;
-  char buf[128], *p;
+  while (read(fd, &de, sizeof(de)) == sizeof(de)) {
+    if (de.inum == 0) continue;
+
+    char name[DIRSIZ + 1];
+    memmove(name, de.name, sizeof(de.name));
+    name[DIRSIZ] = '\0';
+
+    if ((strcmp(name, ".") == 0) || (strcmp(name, "..") == 0)) continue;
+
+    char joined_src[MAX_PATH_LENGTH];
+    if (join_paths(joined_src, sizeof(joined_src), src, name) < 0) return -1;
+
+    char joined_target[MAX_PATH_LENGTH];
+    if (join_paths(joined_target, sizeof(joined_target), target, name) < 0)
+      return -1;
+
+    if (cp(joined_src, joined_target) < 0) return -1;
+  }
+
+  return 0;
+}
+
+static int cp(const char* src, const char* target) {
+  int fd, err;
+  struct stat st;
 
   if ((fd = open(src, O_RDONLY)) < 0) {
-    printf(2, "cant locate %s\n", src);
+    printf(2, "can't locate %s\n", src);
     return -1;
   }
 
@@ -55,53 +111,31 @@ int cp(const char* src, const char* target) {
 
   switch (st.type) {
     case T_FILE:
-      close(fd);
-      copy_file(src, target);
+      err = copy_file(fd, target);
       break;
-
     case T_DIR:
-      strcpy(buf, src);
-      p = buf + strlen(buf) - 1;
-      if (*p != '/') {
-        p++;
-        *p++ = '/';
-      }
-      while (read(fd, &de, sizeof(de)) == sizeof(de)) {
-        if (de.inum == 0) continue;
-        memmove(p, de.name, DIRSIZ);
-        p[DIRSIZ] = 0;
-        if (stat(buf, &st) < 0) {
-          printf(1, "ls: cannot stat %s\n", buf);
-          continue;
-        }
-        if ((strcmp(fmtname(buf), ".") == 0) ||
-            (strcmp(fmtname(buf), "..") == 0)) {
-          continue;
-        }
-        char new_target[128], *t;
-        strcpy(new_target, target);
-        t = new_target + strlen(new_target) - 1;
-        if (*t != '/') {
-          t++;
-          *t++ = '/';
-        }
-        memmove(t, de.name, DIRSIZ);
-        if (st.type == T_DIR) {
-          mkdir(new_target);
-        }
-        cp(buf, new_target);
-      }
-      close(fd);
+      err = copy_dir(fd, src, target);
+      break;
+    default:
+      err = -1;
+      printf(2, "Unsupported file type %d\n", st.type);
       break;
   }
-  return 0;
+
+  close(fd);
+  return err;
 }
 
 int main(int argc, char* argv[]) {
   if (argc != 3) {
-    printf(1, "cp requires two arguemnts, source and target\n");
+    printf(2, "cp requires two arguments: source and target\n");
     exit(1);
   }
-  cp(argv[1], argv[2]);
+
+  const char* src = argv[1];
+  const char* target = argv[2];
+
+  if (cp(src, target) < 0) exit(1);
+
   exit(0);
 }
