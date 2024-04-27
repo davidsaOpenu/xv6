44124b2a3c2010cc0f88786fd74337a65d3ab856
Nahman Khayet <nkhayet@gmail.com>
2024-04-27 22:31:58 +0300

    Added support for cgroup cpu.weight
    With the addition of the cpu.weight, the proportion of cpu time
    a cgroup gets can be changed.
    
    Added test test_limiting_cpu_weight - tests cpu.weight interaction
    with cgfs.
    
    Added test test_cpu_weight - run processes in different cgroups
    with different weights, and test that the cpu was distrebuted
    respectively.
    
    Ported to xv6 repo from xv6-ns.
    Trello card: https://trello.com/c/mV5Q92SJ/143-add-support-for-cgroup-cpuweight
    Original gerrit change (xv6-ns): https://review.gerrithub.io/c/davidsaOpenu/xv6-ns/+/519585
    
    Co-Authored-By: Alon Wartski <alon.w.10@gmail.com>
    
    Change-Id: I71500fc7653b11927ba98adeadda541ad3fd7efd

--- cpu_account.h
+++ cpu_account.h
@@ -8,6 +8,8 @@ struct cpu_account {
   unsigned int cpu_account_period;
   unsigned int cpu_account_frame;
   unsigned int process_cpu_time;
+  unsigned int curr_frame_total_process_cpu_time;
+  unsigned int cpu_id;
   struct cgroup* cgroup;
 };
 
@@ -36,6 +38,14 @@ void cpu_account_schedule_proc_update(struct cpu_account* cpu, struct proc* p);
  */
 int cpu_account_schedule_process_decision(struct cpu_account* cpu,
                                           struct proc* p);
+
+/**
+ * Make a decision whether the given process can be scheduled.
+ * The decision is made according to the cpu weights of the cgroups.
+ * Returns zero for can not schedule or nonzero in case can schecule.
+ */
+int cpu_account_schedule_process_decision_by_weights(struct cpu_account* cpu,
+                                                     struct proc* p);
 
 /**
  * Event callback function to let the cpu account mechanism know that a process
