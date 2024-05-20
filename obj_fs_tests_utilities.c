27fedda7830739d7460cb525a58078e17d0a4b26
Sagi Marom <sagi1.marom@gmail.com>
2024-05-20 19:24:14 +0300

    Revisit fs tests: improve framework, add rm -r, fix CI
    The changes of this commit are:
    
    **Fixes:**
    
    - Improve the tests framework: now it can be used by both user tests and host tests.
      Its code resides now in "tests/framework". A common host test mock was added.
      Future work is still required to convert some user tests to use this framework
      and not "tester.h".
    - Convert the kvector host tests to use the new framework.
    - Fix all objfs host tests.
    - Add kvector and objfs to CI.
    - Support recursive removal of files by the rm userspace utility '-r' flag.
      Also test this feature.
    - Fix CI: When each test ran successfully, running them as part of the CI made
      them fail, as tests didn't clean their files. Clean all tests using the 'rm -r'
      command. Also fixed linter comments of old code.
    - Fix kvector memcmp function, which allows us to compare the content of a kvector
      without allocating memory and copying the kvector content into it.
    
    **Bug Fixes:**
    
    - `kvector.c`: An element size must be less than page size. This was not verified
      by kvector and caused a divide by zero bug. Fix: Verify it.
    - `obj_cache.c`: Wrong parameter passed to `memmove_from_vector()` -- total size
      instead of number of elements. It wasn't discovered before since the usages of
      kvector were wrong -- they used it as an array of elements of size 1 instead of
      an array of elements of struct/object size.
    - `obj_disk.c`: In `add_object()`, the search for free space for the new object
      starts from inode 0, considering the superblock and blocks table, and later on
      the "actual" objects offset in the disk are subtracted from the leftmost
      allocation space found. When the disk is full, it causes a pointer arithmetic
      underflow, resulting in double disk usage (by multiple different objects).
    - `obj_disk.c`: When the objects table is full but there is still space on the
      disk, the table is extended, but the occupied bytes counter is not updated, so
      the statistics are wrong (also affects tests).
    - `obj_disk.c`: Upon adding or resizing an object, there is no verification of
      its size, causing underflow in `find_empty_space()` and thus allocating already
      used disk space.
    - `mkdir.c`: This userspace utility returns 0 even upon failure, making some tests
      pass even when directory creation failed.
    
    Change-Id: Id0b02f96d314018a6f4aac8d08520501cba566ed

--- obj_fs_tests_utilities.c
+++
@@ -1,9 +0,0 @@
-#include "obj_fs_tests_utilities.h"
-
-#include <stdio.h>
-#include <stdlib.h>
-
-void panic(const char* msg) {
-  printf("kernel panic: %s\n", msg);
-  exit(1);
-}
--- obj_fs_tests_utilities.h
+++
@@ -1,6 +0,0 @@
-#ifndef XV6_OBJ_FS_TESTS_UTILITIES_H
-#define XV6_OBJ_FS_TESTS_UTILITIES_H
-
-void panic(const char* msg);
-
-#endif /* XV6_OBJ_FS_TESTS_UTILITIES_H */
