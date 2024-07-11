f9079634290de4d984d7ef10165d599d246b33ff
Aviv Naaman <avivnaaman04@gmail.com>
2024-07-11 19:40:35 +0000

    Support OCI Image mounting as container image
    Added Dockerfiles to build docker and then simple, lightweight  OCI images.
    Those images are then converted to xv6 internal_fs_[abc] format,
    allowing them to be mounted using xv6 existing mount mechansim.
    
    Currently, implemnting a full isolation using a pivot_root-like syscall
    is impossible, so added a simple mount() and chdir() to the pouch
    utility instead, to perform a pivot_root-like action to allow accessing
    the image's contents in the defualt child process dir.
    Also fixed images lookup algorithm to support internal_fs_[abc]
    type of images instead of directories representing images.
    Tests were adapted to the new images format, including container
    startups procedure and image listing tests.
    
    In addition, the CI was adapted to the new images building, using skpoeo
    docker image to convert Docker images to OCI, and setting up the python
    toolchain venv in the docker build configuration, rather than when
    running the tests.
    
    Co-Authured-By: Alon Zini <zini.alon.1412@gmail.com>
    Change-Id: I0c5a9313c4ff7b5d0dd4b4110d08537b63ebd0f3

---
+++ images/oci_image_extractor.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+OUTPUT_DIR=$1
+OCI_IMAGE_DIR=$2
+
+if [ -d $OUTPUT_DIR ]; then
+    rm -rf $OUTPUT_DIR
+fi
+
+echo "Changing dir to $OCI_IMAGE_DIR"
+cd $OCI_IMAGE_DIR
+mkdir -p $OUTPUT_DIR
+
+DIGEST_OF_MANIFEST=$(jq --raw-output '.manifests[] |
+    select(.mediaType != null) |
+    select(.mediaType == "application/vnd.oci.image.manifest.v1+json") |
+    .digest' index.json)
+
+MANIFEST_DIGEST_VAL=$(echo ${DIGEST_OF_MANIFEST}| cut -d':' -f2)
+BLOBS_DIR="blobs/$(echo ${DIGEST_OF_MANIFEST}| cut -d':' -f1)"
+
+echo "manifest.json is at $BLOBS_DIR/$MANIFEST_DIGEST_VAL"
+cd $BLOBS_DIR
+LAYERS_DIGESTS=$(jq --raw-output '.layers[] | .digest' $MANIFEST_DIGEST_VAL)
+LAYERS=$(echo $LAYERS_DIGESTS | cut -d':' -f2)
+echo "Layers are: $LAYERS"
+
+echo "Extracting layers to $OUTPUT_DIR"
+for LAYER in $LAYERS; do
+    cp $LAYER $OUTPUT_DIR/$LAYER.tgz
+    tar --overwrite -xzf $OUTPUT_DIR/$LAYER.tgz -C $OUTPUT_DIR
+    rm $OUTPUT_DIR/$LAYER.tgz
+    echo $LAYER
+done
