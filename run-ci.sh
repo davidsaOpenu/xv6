fbf8998df6b0bf24e1993bb545522bf445dbc380
Nahman Khayet <nkhayet@gmail.com>
2024-10-11 19:32:42 +0300

    Subject: [PATCH] Added support for cgroup cpu.weight
    With the addition of the cpu.weight, the proportion of cpu time
    a cgroup gets can be changed.
    
    Added test test_limiting_cpu_weight - tests cpu.weight interaction
    with cgfs.
    
    Added test test_cpu_weight - run processes in different cgroups
    with different weights, and test that the cpu was distrebuted
    respectively.
    
    Ported to xv6 repo from xv6-ns.
    Trello card: https://trello.com/c/mV5Q92SJ/143-add-support-for-cgroup-cpuweight
    
    from https://review.gerrithub.io/c/davidsaOpenu/xv6-ns/+/519585 also
    Co-Authored-By: Alon Wartski <alon.w.10@gmail.com> and Yuval Zan <zanyuval@gmail.com>
    
    Change-Id: I71500fc7653b11927ba98adeadda541ad3fd7efd

--- run-ci.sh
+++ run-ci.sh
@@ -53,11 +53,11 @@ ERROR_CODE=20
 
 # suppress finding standard include headers, scan only custom header files.
 cppcheck --error-exitcode=${ERROR_CODE} \
-    --inline-suppr --suppress=missingIncludeSystem \
+    --inline-suppr \
     --enable=portability,information,performance,warning --inconclusive \
     --suppress=missingInclude \
     -DSTORAGE_DEVICE_SIZE=1 "-I$(pwd)" \
-    --xml --xml-version=2 . 2> cppcheck.xml || \
+    --xml --xml-version=2 . || \
     { echo "${RED}Failed: please check cppcheck.xml for details.${NC}"; \
     exit 1; }
 # Gerrit pluging creates htmlreports automatically from cppcheck.
