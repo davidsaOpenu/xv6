4070cacf9f6b7400203dd8edab57e6211c4843b5
Aviv Naaman <avivnaaman04@gmail.com>
2024-10-20 05:48:51 +0000

    294: Implement pouch images build
    The pouch build command is now fully working and tested properly.
    The COPY instruction was added to the pouchfile specs to allow a simple
    addition of new files from the root filesystem to the images. IMPORT and
    RUN commands have been implemented too. The built image is usable via
    pouch start on the same image tag, built. Pouch container startup logic
    has been heavily modified to re-use the same container code for pouch
    start/stop and pouch build.
    
    Improved the kernel's refcount on inodes, mounts and devices to make
    error handling more robust. Increased the user mode processes' stack
    size due to pouch requirements (1->4pgs). Added support for `sh -c`,
    improved shell handling of empty lines, fixed bug in echo, and added
    ulib system() function.
    
    Modified the existing tests of pouchfiles to build an actual set of
    pouch images using the current framework. Adapt runtests.exp and add
    tests for invalid syntax, build runtime errors, and valid cases.
    
    Co-Authored-By: Alon Zini <zini.alon.1412@gmail.com>
    
    https://trello.com/c/g7LWz1jl/294-finish-implementing-pouch-build
    Change-Id: Iea40dd185c5e1c4beed6ee73bd7e62856024bc51

--- user/pouch/image.h
+++ user/pouch/image.h
@@ -27,4 +27,19 @@ pouch_status pouch_image_exists(const char* const image_name);
  */
 pouch_status pouch_image_get_path(const char* image_name, char* image_path);
 
+/*
+ *   Copy image from source to target
+ *   @input: source_image_name, target_image_name
+ *   @output: none
+ */
+pouch_status pouch_image_copy(const char* const source_image_name,
+                              const char* const target_image_name);
+
+/*
+ *   Remove image by it's name
+ *   @input: image_name
+ *   @output: none
+ */
+pouch_status pouch_image_rm(const char* const image_name);
+
 #endif  // XV6_USER_POUCH_IMAGE_H
