e1ca06d01198909f4b86582357e19c4f10dffc9a
Aviv Naaman <avivnaaman04@gmail.com>
2024-10-26 14:43:13 +0000

    314: Migrate & update docs
    Migrated the "xv6 containers, namespaces and cgroups" document to a
    markdown format in-repo, including images, charts and code snippet.
    
    The document has been cleaned up and has been tidied-up. Added the new
    pouch build command and a missing pouch images command to the functional
    specification. Re-worked the entire documentation of the pouch utility
    and containers to match the currents spec, including remounting the root
    filesystem, usage of mounts, and the build command implementation.
    
    Added documentation for the pivot root system call.
    
    Added update documentation to refactored devices, mounts, and vfs
    superblock mechanisms.
    
    Sphinx: added a ./reload for development of documentation, added
    spell checker when building the docs.
    
    https://trello.com/c/mR3N6HL5/314-story-8-clean-the-table-and-documentation-md
    
    Co-Authored-By: Alon Zini <zini.alon.1412@gmail.com>
    Change-Id: Id6720ef967ba047d7548ac615f7a331f7cef20bc

--- docs/build.sh
+++ docs/build.sh
@@ -2,10 +2,7 @@
 
 set -e
 
-python -m venv /tmp/virtdocs
 source /tmp/virtdocs/bin/activate
-pip install --upgrade pip
-pip install -r requirements.txt
 cd source && make html 2>&1 | grep -v "WARNING" && \
     [ ${PIPESTATUS[0]} -eq 0 ] && echo "Documentation build successful!" || \
     echo "Documentation build failed!"
--- docs/source/TechnicalSpecification/03-cgroup.md
+++ docs/source/TechnicalSpecification/03-cgroup.md
@@ -1,2 +1,1 @@
-Cgroup
-======
+# cgroups
