d921e0a2bd9fc028c86932f549f57f504e32f0fd
naty55 <natymm314@gmail.com>
2025-08-10 01:34:26 +0300

    cgroup: Add io controller and io.stat support This implements basic io controller operations and the io.stat feature which allows user space to collect stats of io usage like byte count and io ops count
    This patch is originally written for a (much older) version of the
    kernel with a flat file layout and legacy cgroup APIs. It has been
    partially adapted in this commit for the current tree structure.
    
    Trello card: https://trello.com/c/4vKtoSMK/201-added-io-contoller-and-iostat
    
    Change-Id: I1ef1714af1667a0c6df8754f36b367c6b9ca5903

--- tests/xv6/cgroupstests.h
+++ tests/xv6/cgroupstests.h
@@ -2,9 +2,9 @@
 #define XV6_CGROUPSTESTS_H
 
 #define MAX_CONTROLLER_NAME_LENGTH 16
-#define CONTROLLER_COUNT 4
+#define CONTROLLER_COUNT 5
 
-enum controller_types { CPU_CNT, PID_CNT, SET_CNT, MEM_CNT };
+enum controller_types { CPU_CNT, PID_CNT, SET_CNT, MEM_CNT, IO_CNT };
 
 // For memory controler test
 #define KERNBASE \
@@ -47,6 +47,7 @@ enum controller_types { CPU_CNT, PID_CNT, SET_CNT, MEM_CNT };
 #define TEST_1_MEM_MAX "/cgroup/test1/memory.max"
 #define TEST_1_MEM_MIN "/cgroup/test1/memory.min"
 #define TEST_1_MEM_STAT "/cgroup/test1/memory.stat"
+#define TEST_1_IO_STAT "/cgroup/test1/io.stat"
 #define TEST_1_MEM_FAILCNT "/cgroup/test1/memory.failcnt"
 
 #define TEST_2_CGROUP_SUBTREE_CONTROL "/cgroup/test2/cgroup.subtree_control"
