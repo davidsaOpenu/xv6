2ae7d17ad981992fa62f359d0cfa5c747c3c4c93
Ron Shabi <ron@ronsh.net>
2025-09-28 17:22:25 +0300

    Infra: Replace Docker with Podman
    This commit frees us from the dependency of Docker to build OCI images.
    Moreover, now the whole build process can be done without ANY
    containerization.
    
    Just running `make` to build is enough!
    
    Also:
    - Removed race conditions in buildsystem, so now `make -jCPUS` works
    perfectly :)
    
    - Fixed buffer overflow in tests.
    
    - Bypassed some tests which currently do not work.
    
    - Minimized sleep time in Tcl expect script
    
    - Refactored magic numbers in usertests.
    
    Closes: https://trello.com/c/7vcMXs34/341-bid-and-run-tests-of-xv6-on-the-vm-with-podman
    Change-Id: I8cdf2f6dadc813a825b4c2434b10d37a5461bb05
    Signed-off-by: Ron Shabi <ron@ronsh.net>

---
+++ podman-ci.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -exu
+
+SKIP_BUILD=false
+SKIP_GUEST_TESTS=false
+SKIP_HOST_TESTS=false
+HOST_TESTS_DIR="build/tests/host"
+HOST_TESTS=$(find "$HOST_TESTS_DIR" -executable -print)
+
+echo "[Podman CI Starting]"
+
+if [[ ! "$SKIP_BUILD" = "true" ]]; then
+    make -B clean 2>&1          | sed 's/^/[    CLEAN]\t/'
+    make -B 2>&1                | sed 's/^/[     MAKE]\t/'
+    make -B host-tests 2>&1     | sed 's/^/[ MAKEHOST]\t/'
+else
+    echo "[    SKIP] Build"
+fi
+
+# Run Guest Tests
+
+if [[ ! "$SKIP_GUEST_TESTS" = "true" ]]; then
+    ./tests/runtests.exp 2>&1 /dev/null     | sed 's/^/[  EXPECT]\t/'
+else
+    echo "[    SKIP] Guest Tests"
+fi
+
+# Run Host Tests
+
+if [[ ! "$SKIP_HOST_TESTS" = "true" ]]; then
+    for t in $HOST_TESTS; do
+        $t 2>&1     | sed 's/^/[HOSTTESTS]\t/'
+    done
+else
+    echo "[    SKIP] Host Tests"
+fi
