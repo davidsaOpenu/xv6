fdcbcf4a51e60b90b380391ffa743d9691355145
Ron Shabi <ron@ronsh.net>
2025-09-28 17:22:25 +0300

    Infra: Replace Docker with Podman
    This commit frees us from the dependency of Docker to build OCI images.
    Moreover, now the whole build process can be done without ANY
    containerization.
    
    Just running `make` to build is enough!
    
    Also:
    - Removed race conditions in buildsystem, so now `make -jCPUS` works
    perfectly :)
    
    - Fixed buffer overflow in tests.
    
    - Bypassed some tests which currently do not work.
    
    - Minimized sleep time in Tcl expect script
    
    - Refactored magic numbers in usertests.
    
    Closes: https://trello.com/c/7vcMXs34/341-bid-and-run-tests-of-xv6-on-the-vm-with-podman
    Change-Id: I8cdf2f6dadc813a825b4c2434b10d37a5461bb05
    Signed-off-by: Ron Shabi <ron@ronsh.net>

--- kernel/main.c
+++ kernel/main.c
@@ -74,7 +74,7 @@ pde_t entrypgdir[];  // For entry.S
 
 // Start the non-boot (AP) processors.
 static void startothers(void) {
-  extern uchar _binary_entryother_start[], _binary_entryother_size[];
+  extern uchar _binary_build_entryother_start[], _binary_build_entryother_size[];
   uchar *code;
   struct cpu *c;
   char *stack;
@@ -83,7 +83,7 @@ static void startothers(void) {
   // The linker has placed the image of entryother.S in
   // _binary_entryother_start.
   code = P2V(0x7000);
-  memmove(code, _binary_entryother_start, (uint)_binary_entryother_size);
+  memmove(code, _binary_build_entryother_start, (uint)_binary_build_entryother_size);
 
   for (c = cpus; c < cpus + ncpu; c++) {
     if (c == mycpu())  // We've started already.
