0824ff6aab4c8dac8fe3c2386e6c45692ac41968
Ron Shabi <ron@ronsh.net>
2025-09-28 17:22:25 +0300

    Infra: Replace Docker with Podman
    This commit frees us from the dependency of Docker to build OCI images.
    Moreover, now the whole build process can be done without ANY
    containerization.
    
    Just running `make` to build is enough!
    
    Also:
    - Removed race conditions in buildsystem, so now `make -jCPUS` works
    perfectly :)
    
    - Fixed buffer overflow in tests.
    
    - Bypassed some tests which currently do not work.
    
    - Minimized sleep time in Tcl expect script
    
    - Refactored magic numbers in usertests.
    
    - Modernized Tcl/Expect tests, since old tests were timing out
    
    - Deprecated usage of Docker
    
    - Fixed `pouch start` kernel panic bug
    
    - Solved FS bug caused by ELF debug symbols being too large in some
      compiled files. The older builds used `gstabs` which are now a NOP on
      modern GCC builds.
    
    - Added FS visualizer.
    
    - Added Shellcheck linting
    
    Closes: https://trello.com/c/7vcMXs34/341-bid-and-run-tests-of-xv6-on-the-vm-with-podman
    Change-Id: I8cdf2f6dadc813a825b4c2434b10d37a5461bb05
    Signed-off-by: Ron Shabi <ron@ronsh.net>
    
    nossh1
    
    Change-Id: Ic1f70912bec82995c6fa86581ac8ad2c14de7ba3
    
    yeah i think its done
    
    Change-Id: I8b421973a72bfc20d52c8c4b468bc5d0090de678

---
+++ podman.sh
@@ -0,0 +1,113 @@
+#!/usr/bin/env bash
+
+help() {
+cat << EOF
+podman-ci utility
+    <no arguments>          build & test everything
+    --skip-build:           do not build
+    --skip-guest-tests:     do not run guest-tests
+    --skip-host-tests:      do not run host-tests
+    --skip-lint:            do not run linters
+    -h, --help:             show this screen
+EOF
+}
+
+SKIP_LINT=false
+SKIP_BUILD=false
+SKIP_GUEST_TESTS=false
+SKIP_HOST_TESTS=false
+
+
+while [[ "$#" -ne 0 ]]; do
+    case "$1" in
+        --skip-build)
+            SKIP_BUILD=true
+        ;;
+        --skip-lint)
+            SKIP_LINT=true
+        ;;
+        --skip-guest-tests)
+            SKIP_GUEST_TESTS=true
+        ;;
+        --skip-host-tests)
+            SKIP_HOST_TESTS=true
+        ;;
+        --help | -h)
+            help
+            exit
+        ;;
+    esac
+
+    shift
+done
+
+echo "[Podman CI Starting]"
+
+lint_shell() {
+    if ! command -v shellcheck; then
+        echo "Skipping linting: shellcheck is not installed"
+        return 1
+    fi
+
+    shell_scripts=$(find . -name '*.sh'  -not -path './docs/*' -not -path './build/*')
+    for script in $shell_scripts; do
+        shellcheck "$script"          | sed "s/^/[SHELLCHECK]\t/";
+
+        # shellcheck disable=SC2181 # Because of sed
+        if [ "$?" -ne 0 ]; then
+            echo "FAILED ON SHELLCHECK: $script"
+            exit 1
+        fi
+    done
+}
+
+lint_clang_format() {
+    if ! command -v clang-format-16; then
+        echo "Skipping linting: clang-format-16 is not installed"
+        return 1
+    fi
+
+    source_files=$(find . -name '*.[ch]'  -not -path './docs/*' -not -path './build/*')
+
+    for f in $source_files; do
+        clang-format-16 -i --verbose "$f" 2>&1 1>/dev/null | sed "s/^/[CLANG-FORMAT]\t/";
+    done
+}
+
+if [[ ! "$SKIP_LINT" = "true" ]]; then
+    echo "[    LINT] Shell Scripts"
+    lint_shell
+    echo "[    LINT] Shell Scripts OK"
+    echo "[    LINT] Clang-Format"
+    lint_clang_format
+    echo "[    LINT] Clang-Format OK"
+fi
+
+if [[ ! "$SKIP_BUILD" = "true" ]]; then
+    make -B clean 2>&1          | sed 's/^/[    CLEAN]\t/'
+    make -B 2>&1                | sed 's/^/[     MAKE]\t/'
+    make -B host-tests 2>&1     | sed 's/^/[ MAKEHOST]\t/'
+else
+    echo "[    SKIP] Lint"
+fi
+
+# Run Guest Tests
+
+if [[ ! "$SKIP_GUEST_TESTS" = "true" ]]; then
+    expect ./tests/run-guest-tests.exp 2>&1 | sed 's/^/[  EXPECT]\t/'
+else
+    echo "[    SKIP] Guest Tests"
+fi
+
+# Run Host Tests
+
+if [[ ! "$SKIP_HOST_TESTS" = "true" ]]; then
+
+HOST_TESTS_DIR=$(realpath "build/tests/host")
+HOST_TESTS=$(find "$HOST_TESTS_DIR" -executable -print)
+    for t in $HOST_TESTS; do
+        $t 2>&1     | sed 's/^/[HOSTTESTS]\t/'
+    done
+else
+    echo "[    SKIP] Host Tests"
+fi
