f9e526278d427ae8336c78bd34891d367a4b3166
Ron Shabi <ron@ronsh.net>
2025-09-28 17:22:25 +0300

    Infra: Replace Docker with Podman
    This commit frees us from the dependency of Docker to build OCI images.
    Moreover, now the whole build process can be done without ANY
    containerization.
    
    Just running `make` to build is enough!
    
    Also:
    - Removed race conditions in buildsystem, so now `make -jCPUS` works
    perfectly :)
    
    - Fixed buffer overflow in tests.
    
    - Bypassed some tests which currently do not work.
    
    - Minimized sleep time in Tcl expect script
    
    - Refactored magic numbers in usertests.
    
    - Modernized Tcl/Expect tests, since old tests were timing out
    
    Closes: https://trello.com/c/7vcMXs34/341-bid-and-run-tests-of-xv6-on-the-vm-with-podman
    Change-Id: I8cdf2f6dadc813a825b4c2434b10d37a5461bb05
    Signed-off-by: Ron Shabi <ron@ronsh.net>

--- kernel/proc.c
+++ kernel/proc.c
@@ -138,7 +138,7 @@ found:
 //  Set up first user process.
 void userinit(void) {
   struct proc *p;
-  extern char _binary_initcode_start[], _binary_initcode_size[];
+  extern char _binary_build_initcode_start[], _binary_build_initcode_size[];
 
   // Initialize the root cgroup.
   cgroup_initialize(cgroup_root(), 0, 0);
@@ -147,7 +147,7 @@ void userinit(void) {
 
   initproc = p;
   if ((p->pgdir = setupkvm()) == 0) panic("userinit: out of memory?");
-  inituvm(p->pgdir, _binary_initcode_start, (int)_binary_initcode_size);
+  inituvm(p->pgdir, _binary_build_initcode_start, (int)_binary_build_initcode_size);
   p->sz = PGSIZE;
   memset(p->tf, 0, sizeof(*p->tf));
   p->tf->cs = (SEG_UCODE << 3) | DPL_USER;
